296f74ced6b793903f1b4d41aa4485ff07af4de6
ryan g <ryan@duckz.local>
2014-03-04 14:41:08 -0800

    print binary

---
+++ src/c5/StringToBinary.java
@@ -0,0 +1,27 @@
+package c5;
+
+public class StringToBinary {
+
+	
+	public static void printBinary(String s) {
+		
+		Integer data = Integer.parseInt(s);
+		
+		
+		for(int i=32; i>=0; i--) {
+			if  (( data & ( 1 << i ) ) != 0 ) {
+				System.out.print(1);
+			} else {
+				System.out.print(0);
+			}
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		String s = "2";
+		printBinary(s);
+		
+	}
+	
+}
